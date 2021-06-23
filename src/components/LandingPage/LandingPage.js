a52623d4f5020be331cb03bb593a60ca257d9398
Jugg1 <adeoluwaadeboye@gmail.com>
2021-06-23 10:45:44 +0100

    :tada: project takeoff

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./landingpage.css";
+
+const LandingPage = () => {
+	return (
+		<>
+			<div className="container">
+				{/* Nav Area with Search */}
+				<div className="row">
+					<div className="col-sm-6">
+						<div></div>
+					</div>
+					<div className="col-sm-6">
+						<div></div>
+					</div>
+				</div>
+
+				{/* Sub Heading Row */}
+				<div className="row">
+					<div className="col-sm-6">
+						<div></div>
+					</div>
+					<div className="col-sm-6">
+						<div></div>
+					</div>
+				</div>
+
+				{/* Property Cards */}
+				<div className="row"></div>
+			</div>
+		</>
+	);
+};
+
+export default LandingPage;
